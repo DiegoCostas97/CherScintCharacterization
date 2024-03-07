21652c3f570b67987b30c0b2f34a5ef6eca2d60b
DiegoCostas97 <diecosro@gmail.com>
2024-03-07 15:12:40 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# CherScintCharacterizatioin
+# CherScintCharacterization
+
+The code performs an early Cherenkov and Scintillation light analysis for a WCSim simulation output file.
+
+- first.py
+This file has all the needed functions for the analysis.
+
+- second.py
+Runs the functions inside first.py and performs the actual analysis. This will return
+    - Checks
+        - Neutron energy distribution plot.
+        - Gamma energy distribution plot.
+        - Number of events in which the neutron actually gets captured.
+        - Trigger times for DigiHits and TrueHits.
+    - Analysis
+        - Scintillation light coming from the Tag Gamma-Ray plot + info.
+        - Cherenkov light coming from the nCapture Gamma-Ray plot + info.
+        - Reconstruction variables (inputs + possible targets) stored in file.
+
+Please note that this relies on a previous set of functions, contained inside the WCSimFilePackages repository.
+
+In order to run the analysis, just 
+```
+python second.py
+```
+
+Please note that you'll need to modify the path for the "paquetes" library (i.e. WCSimFilePackages)
+as well as the path for `first.py`.
+
+Require dependecies are:
+```
+numpy
+matplotlib
+sys
+pickle
+pandas
+tqdm 
+PdfPages
+```
--- first.py
+++ first.py
@@ -5,13 +5,7 @@ import matplotlib.pyplot as plt
 import sys
 import pickle
 
-path_to_paquetes = "/Users/diiego/Library/Mobile Documents/com~apple~CloudDocs/Desktop/DIEGO_cloud/USC/PHD/HK/HK SOURCES/code/ambe_source/npz_ana/paquetes"
-sys.path.append(path_to_paquetes)
-
 # Import Additional Tools
-from npz_to_df                       import truehits_info_to_df
-from npz_to_df                       import simple_track_info_to_df
-from npz_to_df                       import digihits_info_to_df
 from tqdm                            import tqdm
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -152,55 +146,55 @@ def plot_light(data, bins, gamma_type, light_type, events, threshold, sfm, path,
 # Selection of the Cherenkov Photons from the nCapture Gamma-ray functions
 
 def electrons_from_Cherenkov(df, nevents):
-    
+
     # Create a dictionary where the key is the event number and the info are the electrons that produced 
     # Cherenkov light that produced hits in our PMTs.
-    
+
     # Initialize the dictionary that's gonna record the info
     electrons_that_produce_cherenkov = {}
-    
+
     # Loop over all events
     for i in nevents:
-        
+
         # If the event only has one track, append the track parent in a special way for the code not to break
         if len(df[df['event_id'].values == i]) == 1:
             electrons_that_produce_cherenkov[i] = [df['true_hit_parent'][i]]
-        
+
         # In every other scenario, append the track parents unique appearances
         else:
             electrons_that_produce_cherenkov[i] = list(np.unique(df['true_hit_parent'][i].values))
-    
+
     return electrons_that_produce_cherenkov
 
 def real_cherenkov_electrons(df, e, nevents):
-    
+
     # Create a dictionary where the key is the event number and the info are the electron which produced
     # Cherenkov light (that produced hits) that comes from the nCapture
-    
+
     # Initialize the dictionary
     el = {}
-    
+
     # Loop over the events in which we have nCapture and Cherenkov
     for i in tqdm(nevents, desc="Running real_cherenkov_electrons", unit="iter", dynamic_ncols=True):
         # Create a sub-DataFrame with the data of the current event.
         # This incredibly speeds up the process 
         temp_df = df[df['event_id'].values == i]
-        
+
         # Look for the track ID of the nCapture Gamma in this event
         temp_ncID_df = temp_df[(temp_df['track_pid'].values == 22)]
         ncID         = temp_ncID_df[(temp_ncID_df['track_creator_process'].values == "nCapture")]['track_id'].values[0]
-        
+
         # Take only the electrons which have been produced by the nCapture gamma
         temp_df = temp_df[(temp_df['track_id'].isin(e[i]))]
         el[i] = sorted(temp_df[(temp_df['track_parent'].values == ncID)]['track_id'].values)
-        
+
     re = {}
     for i in nevents:
         if len(el[i]) == 0:
             pass
         else:
             re[i] = el[i]
-    
+
     return re
 
 ###################################################################################################
@@ -214,20 +208,20 @@ def anyCherenkov_info(e, df):
 
     for i in tqdm(e.keys(), desc="Running anyCherenkov info", unit="iter", dynamic_ncols=True):
         count = 0
-        
+
         temp_df = df[df['event_id'].values == i]
-        
+
         for j in temp_df['digi_hit_truehit_parent_trackID']:
             for k in e[i]:
                 if temp_df.notna().any()[1] and k in j:
                     count += 1
-        
+
         if count != 0:
             events_anyCher.append(i)
             counts_anyCher.append(count)
-    
+
     return events_anyCher, counts_anyCher
-            
+
 def nCapture_Cherenkov_info(e, df):
     events_nCCher  = []
     counts_nCCher  = []
@@ -238,7 +232,7 @@ def nCapture_Cherenkov_info(e, df):
         count = 0
         # Create a sub-DataFrame of the current event
         temp_df = df[df['event_id'].values == i]
-        
+
         # Loop over the hit parents that created the DigiHits and the index
         for j, l in zip(temp_df['digi_hit_truehit_parent_trackID'], temp_df['digi_hit_truehit_parent_trackID'].index):
             # Loop over the electrons created by the nCapture gamma in this event
@@ -247,12 +241,12 @@ def nCapture_Cherenkov_info(e, df):
                 if temp_df.notna().any()[1] and k in j:
                     indices_nCCher.append(l)
                     count += 1
-        
+
         # If we are in an event we want to append, do it and print info           
         if count != 0:
             events_nCCher.append(i)
             counts_nCCher.append(count)
-        
+
     return events_nCCher, counts_nCCher, indices_nCCher
 
 ###################################################################################################
@@ -263,7 +257,7 @@ def nCapture_Cherenkov_info(e, df):
 # We can do this with the df_final_cher, which is the DF of those events which we can actually tag
 # Or we can do it simply with df_cher, which is the DF of every event with cher light from the nCapture gamma 
 # (for the sake of more training data)
-def output_reconstruction_variables(light_df, track_df):    
+def output_reconstruction_variables(light_df, track_df):
     # Input
     charge   = []
     time     = []
@@ -274,25 +268,25 @@ def output_reconstruction_variables(light_df, track_df):
     gamma_cre_vertex = []
     neutr_int_vertex = []
 
-    gamma_df = track_df[(track_df['track_creator_process'].values == 'nCapture') & 
+    gamma_df = track_df[(track_df['track_creator_process'].values == 'nCapture') &
                             (track_df['track_pid'].values == 22)]
 
     for i in tqdm(np.unique(light_df['event_id']), desc="Outputting Reconstruction Variables", unit="iter", dynamic_ncols=True):
         # Input
         temp_df = light_df[light_df['event_id'].values == i]
-        
+
         charge.append(list(temp_df['digi_hit_charge']))
         time.append(list(temp_df['digi_hit_time'].values))
         position.append(list(temp_df['digi_hit_r'].values))
-    
+
         # Output
         temp_df_gamma = gamma_df[(gamma_df['event_id'].values == i)]
-        
+
         gamma_int_vertex.append(temp_df_gamma['track_rf'].values[0])
         gamma_cre_vertex.append(temp_df_gamma['track_ri'].values[0])
-        
+
         temp_df_neutr = track_df[track_df['event_id'].values == i]
-        
+
         neutr_int_vertex.append(temp_df_neutr[(temp_df_neutr['track_id'].values == 1)]['track_rf'].values[0])
 
     return charge, time, position, gamma_int_vertex, gamma_cre_vertex, neutr_int_vertex
@@ -312,13 +306,13 @@ def writeTriggerTimesPDF(nevents, df, path):
             dhS = []
             thS = []
             for i in triggers:
-                dhS.append(df[(df['event_id'].values == event) & 
+                dhS.append(df[(df['event_id'].values == event) &
                                     (df['digi_hit_trigger'].values == i)]['digi_hit_time'].values)
-                thS.append([i[0] for i in df[(df['event_id'].values == event) & 
+                thS.append([i[0] for i in df[(df['event_id'].values == event) &
                                     (df['digi_hit_trigger'].values == i)]['digi_hit_truehit_times'].values])
-            
+
             trigger_list = ["Trigger_"+str(i) for i in triggers]
-            
+
             fig = plt.figure(figsize=(7,7))
 
             ax1 = fig.add_subplot(211)
@@ -339,95 +333,3 @@ def writeTriggerTimesPDF(nevents, df, path):
             plt.close()
 
     return 0
-
-""" # Path to the npz file in our machine
-npz = '/Users/diiego/Desktop/install/wcsim.npz'
-
-threshold = 2   #/DAQ/TriggerNDigits/Threshold set in macros/daq.mac. Needed for the plots.
-sfm       = 0   #/DAQ/TriggerSaveFailures/Mode set in macros/daq.mac. Needed for the plots.
-nevents   = 100 # Number of events in the .npz
-
-# Creation of the three main DataFrames: trueHits, digiHits and tracks
-df_trueHits       = truehits_info_to_df(npz)
-df_digiHits       = digihits_info_to_df(npz, nevents)
-df_simple_track   = simple_track_info_to_df(npz) # This is simpler and less memory consuming version
-                                                 # of the tracks_df. Please note there is a more complex
-                                                 # version in "paquetes" under the name of track_info_to_df
-
-# Selection of every Scintillation Photon
-events_with_Scintillation = np.unique(df_simple_track[df_simple_track['track_creator_process'] == 'Scintillation']['event_id'].to_numpy())
-sHits_Tag_temp            = df_trueHits[(df_trueHits['event_id'].isin(events_with_Scintillation))]
-sHits_Tag                 = df_trueHits[(df_trueHits['true_hit_creatorProcess'].values == 'Scintillation')]
-
-# Select only those events in which we have Scintillation so we don't loop over the hole dataset
-scint_nevents = list(sHits_Tag.groupby('event_id').count().index.values)
-
-# Call the functions and store the results in the main variable electrons_from_tagGamma
-electrons_that_produce_scintillation = electrons_from_Scintillation(sHits_Tag, scint_nevents)
-electrons_from_tagGamma              = real_scintillation_electrons(df_simple_track, electrons_that_produce_scintillation, scint_nevents)
-print(" ")
-
-
-events_scint, counts_scint, indices_scint = scintillation_info(electrons_from_tagGamma, df_digiHits)
-print(" ")
-
-print("In {} events we have DigiHits produced by the Scintillation light from the Tag gamma".format(len(events_scint)))
-print("Maximum number of DigiHits per event is {}".format(np.max(counts_scint)))
-print("Average number of DigiHits per event is {:.0f}".format(np.mean(counts_scint)))
-print(" ")
-
-# Selection of every Cherenkov Photon
-events_with_nCapture = np.unique(df_simple_track[df_simple_track['track_creator_process'] == 'nCapture']['event_id'].to_numpy())
-temp_cHits_nCapture = df_trueHits[df_trueHits['event_id'].isin(events_with_nCapture)]
-cHits_nCapture      = temp_cHits_nCapture[temp_cHits_nCapture['true_hit_creatorProcess'].values == 'Cerenkov']
-
-# Select only those events in which we have Cherenkov so we don't loop over the hole dataset
-cher_nevents = cHits_nCapture.groupby('event_id').count().index.values
-
-# Call the functions and store the results in the main variable electrons_from_nCapture
-electrons_that_produce_cherenkov = electrons_from_Cherenkov(cHits_nCapture, cher_nevents)
-electrons_from_nCapture          = real_cherenkov_electrons(df_simple_track, electrons_that_produce_cherenkov, cher_nevents)
-print(" ")
-
-events_anyCher, counts_anyCher = anyCherenkov_info(electrons_that_produce_cherenkov, df_digiHits)
-print(" ")
-print("In {} events we have DigiHits produced by the Cherenkov light from any gamma".format(len(events_anyCher)))
-print("Maximum number of DigiHits per event is {}".format(np.max(counts_anyCher)))
-print(" ")
-
-events_nCCher, counts_nCCher, indices_nCCher = nCapture_Cherenkov_info(electrons_from_nCapture, df_digiHits)
-
-print("In {} events we have DigiHits produced by the Cherenkov light from the nCapture gamma".format(len(events_nCCher)))
-print("Maximum number of DigiHits per event is {}".format(np.max(counts_nCCher)))
-
-# Plot
-plot_light(counts_scint, "Tag", "Scintillation", events_scint)
-plot_light(counts_nCCher, "nCapture", "Cherenkov", events_nCCher)
-
-# Creating a new DigiHits DataFrame just with the Tag and nCapture light
-df_cher  = df_digiHits.loc[indices_nCCher]
-df_scint = df_digiHits.loc[indices_scint]
-
-cher_events  = np.unique(df_cher['event_id'])
-scint_events = np.unique(df_scint['event_id'])
-
-common_evts = [i for i in np.unique(df_digiHits['event_id']) if i in cher_events and i in scint_events]
-print("There are {} events with both Tag and nCapture light".format(len(common_evts)))
-print(" ")
-cher_indices  = df_cher[df_cher['event_id'].isin(common_evts)].index.to_numpy()
-scint_indices = df_scint[df_scint['event_id'].isin(common_evts)].index.to_numpy()
-
-final_indices = np.sort(np.concatenate([cher_indices, scint_indices]))
-
-df_final = df_digiHits.loc[final_indices]
-df_final_cher = df_cher[df_cher['event_id'].isin(common_evts)]
-
-# Output Reconstruction Variables
-charge, time, position, gamma_int_vertex, gamma_cre_vertex, neutr_int_vertex = output_reconstruction_variables(df_cher, df_simple_track)
-
-# Save the variables in a file
-with open('./reconstruction_variables.pkl', 'wb') as file:
-    pickle.dump([charge, time, position, gamma_int_vertex, gamma_cre_vertex, neutr_int_vertex], file)
-
-# Just write that many events as you want to inspect, default is 30
-writeTriggerTimesPDF(30) """
